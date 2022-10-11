dd675d0663e246345d4257400aa9f0138857a755
Stanisław Franczyk <sfranczyk5@gmail.com>
2022-10-11 19:25:34 +0100

    Add Photo model

---
+++ lib/models/photo.dart
@@ -0,0 +1,11 @@
+class Photo {
+  String author;
+  String name;
+  String location;
+  int likes = 0;
+  String info;
+  String urlImage;
+
+  Photo(this.author, this.name, this.location, this.likes, this.info,
+      this.urlImage);
+}
